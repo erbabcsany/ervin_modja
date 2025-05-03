d58663380dfb477140de115fd5bffdc69b647f7d
Babcsány Ervin <ervin@hinfox.com>
2025-05-03 23:31:42 +0200

    1.20.1 (47.0.0) ref type 3

--- src/main/java/com/babcsany/minecraft/ervin_modja/ErvinModja.java
+++ src/main/java/com/babcsany/minecraft/ervin_modja/ErvinModja.java
@@ -3,14 +3,17 @@ package com.babcsany.minecraft.ervin_modja;
 import com.babcsany.minecraft.ervin_modja.registries.ModRegistries;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,7 +31,7 @@ public class ErvinModja {
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "ervin_modja";
 
-    public ErvinModja() {
+    public ErvinModja(/*FMLJavaModLoadingContext context*/) {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the mod objects for mod-loading
@@ -44,12 +47,26 @@ public class ErvinModja {
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+//        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
+//        // Some common setup code
+//        LOGGER.info("HELLO FROM COMMON SETUP");
         // some pre-init code
         LOGGER.info("HELLO FROM PRE-INIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getDescriptionId());
+
+//        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+//
+//        if (Config.logDirtBlock)
+//            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+//
+//        LOGGER.info("{}{}", Config.magicNumberIntroduction, Config.magicNumber);
+//
+//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
@@ -92,5 +109,18 @@ public class ErvinModja {
     public void onServerStarting(ServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
     }
 }
--- src/main/java/com/babcsany/minecraft/ervin_modja/registries/ModRegistries.java
+++ src/main/java/com/babcsany/minecraft/ervin_modja/registries/ModRegistries.java
@@ -3,6 +3,10 @@ package com.babcsany.minecraft.ervin_modja.registries;
 import com.babcsany.minecraft.ervin_modja.ErvinModja;
 import com.babcsany.minecraft.ervin_modja.registries.init.BlockInit;
 import com.babcsany.minecraft.ervin_modja.registries.init.ItemInit;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -10,21 +14,32 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import static com.babcsany.minecraft.ervin_modja.ErvinModja.MOD_ID;
+
 public class ModRegistries {
+    // Create a Deferred Register to hold Blocks which will all be registered under the "ervin_modja" namespace
     public static final DeferredRegister<Block> BLOCKS = create(ForgeRegistries.BLOCKS);
+    // Create a Deferred Register to hold Items which will all be registered under the "ervin_modja" namespace
     public static final DeferredRegister<Item> ITEMS = create(ForgeRegistries.ITEMS);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "ervin_modja" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = create(Registries.CREATIVE_MODE_TAB);
 
     ModRegistries() {}
 
     private static <B> DeferredRegister<B> create(IForgeRegistry<B> reg) {
-        return DeferredRegister.create(reg, ErvinModja.MOD_ID);
+        return DeferredRegister.create(reg, MOD_ID);
+    }
+
+    private static <B> DeferredRegister<B> create(ResourceKey<? extends Registry<B>> reg) {
+        return DeferredRegister.create(reg, MOD_ID);
     }
 
     public static void register(IEventBus modEventBus) {
-        ErvinModja.LOGGER.info(ModRegistries.class);
-        BLOCKS.register(modEventBus);
-        ITEMS.register(modEventBus);
         BlockInit.register(ErvinModja.LOGGER);
         ItemInit.register(modEventBus, ErvinModja.LOGGER);
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+        ErvinModja.LOGGER.info(ModRegistries.class);
     }
 }
